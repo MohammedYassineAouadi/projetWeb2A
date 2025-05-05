f2640847d876aae70dcb491d0b562f95e46fa999
mariem_hamdi <mariem.hamdi@esprit.tn>
2025-05-05 22:02:51 +0200

    first

---
+++ MVC/config.php
@@ -0,0 +1,24 @@
+<?php
+class Config {
+    private static $pdo = null;
+
+    public static function getConnexion() {
+        if (!isset(self::$pdo)) {
+            try {
+                self::$pdo = new PDO(
+                    'mysql:host=localhost;dbname=startup',
+                    'root', // ton utilisateur MySQL (XAMPP par défaut)
+                    '',     // mot de passe vide (par défaut dans XAMPP)
+                    [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                    ]
+                );
+            } catch (PDOException $e) {
+                die('Erreur de connexion : ' . $e->getMessage());
+            }
+        }
+        return self::$pdo;
+    }
+}
+?>
